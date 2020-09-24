7499ffa5f2d99ddbe7aff51111a89471319999a2
abcdra <abdraloguzov@mail.ru>
2020-09-24 18:49:36 +0500

    update PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const session = require("express-session")
 const Pather= require("./Pather")
 
-const PORT = 80
+const PORT = process.env.PORT || 80
 
 const app = express()
 const pather = new Pather(app)
